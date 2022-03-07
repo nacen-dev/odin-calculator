48169555d3028681de0399a0268aa8c47436d454
nacen.dev <nacen.dev@gmail.com>
2022-03-07 22:25:24 +0800

    Create initial operation functions

---
+++ index.js
@@ -0,0 +1,27 @@
+const add = (firstNum, secondNum) => {
+  return firstNum + secondNum;
+};
+
+const subtract = (firstNum, secondNum) => {
+  return firstNum - secondNum;
+};
+
+const multiply = (firstNum, secondNum) => {
+  return firstNum * secondNum;
+};
+
+const divide = (firstNum, secondNum) => {
+  return firstNum / secondNum;
+};
+
+const operate = (operator, firstNum, secondNum) => {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  } else if (operator === "/") {
+    return divide(firstNum, secondNum)
+  } else if (operator === "*") {
+    return multiply(firstNum, secondNum)
+  }
+}

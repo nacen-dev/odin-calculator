f2cd0f9b3da826eae0fafa8d94cdcd81d0d5537f
nacen.dev <nacen.dev@gmail.com>
2022-03-08 17:00:10 +0800

    Create clear function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let firstNum = 0;
+let secondNum = 0;
+let currentOperator = "";
+let displayValue = 10;
+
+
 const add = (firstNum, secondNum) => {
   return firstNum + secondNum;
 };
@@ -20,8 +26,38 @@ const operate = (operator, firstNum, secondNum) => {
   } else if (operator === "-") {
     return subtract(firstNum, secondNum);
   } else if (operator === "/") {
-    return divide(firstNum, secondNum)
+    return divide(firstNum, secondNum);
   } else if (operator === "*") {
-    return multiply(firstNum, secondNum)
+    return multiply(firstNum, secondNum);
   }
-}
+};
+
+const addToDisplay = (display, value) => {};
+
+const clear = () => {
+  firstNum = 0;
+  secondNum = 0;
+  currentOperator = "";
+  displayValue = 0;
+  display.textContent = displayValue;
+};
+
+const display = document.querySelector(".display");
+const clearButton = document.querySelector("#clear");
+
+
+display.textContent = displayValue;
+
+clearButton.addEventListener("click", clear)
+
+document.addEventListener("keydown", (event) => {
+  console.log(event)
+  switch(event.key) {
+    case "c":
+    case "C":
+      clear();
+      return;
+    default:
+      return;
+  }
+})

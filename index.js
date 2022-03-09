f83c911b3d1b1bf078f517db36d2636ebcae8f62
nacen.dev <nacen.dev@gmail.com>
2022-03-09 20:52:40 +0800

    Add calculator button event listener

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let firstNum = 0;
 let secondNum = 0;
 let currentOperator = "";
-let displayValue = 10;
-
+let displayValue = "0";
 
 const add = (firstNum, secondNum) => {
   return firstNum + secondNum;
@@ -22,37 +21,91 @@ const divide = (firstNum, secondNum) => {
 
 const operate = (operator, firstNum, secondNum) => {
   if (operator === "+") {
-    return add(firstNum, secondNum);
+    return add(Number(firstNum), Number(secondNum));
   } else if (operator === "-") {
-    return subtract(firstNum, secondNum);
+    return subtract(Number(firstNum), Number(secondNum));
   } else if (operator === "/") {
-    return divide(firstNum, secondNum);
+    return divide(Number(firstNum), Number(secondNum));
   } else if (operator === "*") {
-    return multiply(firstNum, secondNum);
+    return multiply(Number(firstNum), Number(secondNum));
   }
+  return;
 };
 
-const addToDisplay = (display, value) => {};
+const isAnOperator = (value) => {
+  return /[+-/*]/.test(value);
+};
+
+const isANumber = (value) => {
+  return /\d/.test(value);
+};
+
+const addToDisplay = (value) => {
+  console.log("value", value);
+  console.log("display", displayValue);
+  if (isAnOperator(value)) {
+    if (firstNum && currentOperator && secondNum) {
+      displayValue = operate(currentOperator, firstNum, secondNum);
+      firstNum = displayValue;
+      currentOperator = value;
+      secondNum = displayValue;
+    } else if (!currentOperator) {
+      currentOperator = value;
+      firstNum = displayValue;
+    } else if (!secondNum) {
+      secondNum = displayValue;
+      displayValue = operate(currentOperator, firstNum, secondNum);
+      firstNum = displayValue;
+      secondNum = displayValue;
+      currentOperator = value;
+    }
+  } else if (value === "=") {
+    if (firstNum && currentOperator && secondNum) {
+      displayValue = operate(currentOperator, firstNum, secondNum);
+      firstNum = displayValue;
+      secondNum = 0;
+      currentOperator = "";
+    }
+  } else if (value === ".") {
+    if (displayValue.includes(".")) return;
+    else {
+      displayValue += value;
+    }
+  } else if (isANumber(value)) {
+    if (displayValue === "0") {
+      displayValue = value;
+    } else if (firstNum && currentOperator && !secondNum) {
+      displayValue = value;
+      secondNum = displayValue;
+    } else displayValue += value;
+  }
+};
 
 const clear = () => {
   firstNum = 0;
   secondNum = 0;
   currentOperator = "";
-  displayValue = 0;
+  displayValue = "0";
   display.textContent = displayValue;
 };
 
 const display = document.querySelector(".display");
 const clearButton = document.querySelector("#clear");
-
+const calculatorButtons = document.querySelectorAll(".calculator-button");
 
 display.textContent = displayValue;
 
-clearButton.addEventListener("click", clear)
+clearButton.addEventListener("click", clear);
+calculatorButtons.forEach((calculatorButton) => {
+  calculatorButton.addEventListener("click", (evt) => {
+    // console.log(evt.target.value)
+    addToDisplay(evt.target.value);
+    display.textContent = displayValue;
+  });
+});
 
 document.addEventListener("keydown", (event) => {
-  console.log(event)
-  switch(event.key) {
+  switch (event.key) {
     case "c":
     case "C":
       clear();
@@ -60,4 +113,4 @@ document.addEventListener("keydown", (event) => {
     default:
       return;
   }
-})
+});
